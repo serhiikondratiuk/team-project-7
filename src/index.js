13f52a1b844bb847050a72ab4939ccf6fe300850
MariaShebeko <marygro89@gmail.com>
2021-07-04 14:45:10 +0300

    Убрала библиотеки с гамбургером. Добавлена иконки мобильного меню

--- src/index.js
+++ src/index.js
@@ -1,32 +1,18 @@
 import './sass/main.scss';
 
+  
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
   const mobileBtnClose = document.querySelector("[data-menu-close]");
 
-  // var hamburger = document.querySelector(".hamburger");
-
   menuBtnRef.addEventListener("click", () => {
     mobileMenuRef.classList.toggle("is-open");
-    menuBtnRef.classList.toggle("is-active");
   });
 
-  // mobileBtnClose.addEventListener("click", () => {
-  //   mobileMenuRef.classList.toggle("is-open");
-  //   menuBtnRef.classList.toggle("is-active");
-  // });
-
-  //  hamburger.addEventListener("click", function() {
-  //   // Toggle class "is-active"
-  //   hamburger.classList.toggle("is-active");
-  //   // Do something else, like open/close menu
-  //  });
-  
+  mobileBtnClose.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open");
+  });
 })();
 
-
-  // Look for .hamburger
-  
-  // On click
  
