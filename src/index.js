728f8b1d7da7ac0bbc964aa331d3dd79e1b6cec1
MariaShebeko <marygro89@gmail.com>
2021-07-04 13:08:20 +0300

    trial to do a burger-button

--- src/index.js
+++ src/index.js
@@ -5,21 +5,28 @@ import './sass/main.scss';
   const mobileMenuRef = document.querySelector("[data-menu]");
   const mobileBtnClose = document.querySelector("[data-menu-close]");
 
+  // var hamburger = document.querySelector(".hamburger");
+
   menuBtnRef.addEventListener("click", () => {
     mobileMenuRef.classList.toggle("is-open");
+    menuBtnRef.classList.toggle("is-active");
   });
 
-  mobileBtnClose.addEventListener("click", () => {
-    mobileMenuRef.classList.toggle("is-open");
-  });
+  // mobileBtnClose.addEventListener("click", () => {
+  //   mobileMenuRef.classList.toggle("is-open");
+  //   menuBtnRef.classList.toggle("is-active");
+  // });
+
+  //  hamburger.addEventListener("click", function() {
+  //   // Toggle class "is-active"
+  //   hamburger.classList.toggle("is-active");
+  //   // Do something else, like open/close menu
+  //  });
+  
 })();
 
 
   // Look for .hamburger
-  var hamburger = document.querySelector(".hamburger");
+  
   // On click
-  hamburger.addEventListener("click", function() {
-    // Toggle class "is-active"
-    hamburger.classList.toggle("is-active");
-    // Do something else, like open/close menu
-  });
+ 
