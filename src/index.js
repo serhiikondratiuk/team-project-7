1e5db94b7e534ea4e0c574bdc72fc5645fe956c2
MariaShebeko <marygro89@gmail.com>
2021-07-04 10:27:37 +0300

    Добавлена корявая кнопка бургера и анимация на выплывающее меню

--- src/index.js
+++ src/index.js
@@ -13,3 +13,13 @@ import './sass/main.scss';
     mobileMenuRef.classList.toggle("is-open");
   });
 })();
+
+
+  // Look for .hamburger
+  var hamburger = document.querySelector(".hamburger");
+  // On click
+  hamburger.addEventListener("click", function() {
+    // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
+    // Do something else, like open/close menu
+  });
