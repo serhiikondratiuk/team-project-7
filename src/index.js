84c05520e3dbe591f66baa6eef52da2a763835a7
Serhii Kondratiuk <sergey932015@gmail.com>
2021-07-05 18:06:03 +0300

    test/script/howwedo

--- src/index.js
+++ src/index.js
@@ -14,4 +14,17 @@ import './sass/main.scss';
   });
 })();
 
+// script
+function Spoiler() {
+	var ele = document.getElementById("contentSpoiler");
+	var text = document.getElementById("linkSpoiler");
+	if(ele.style.display == "block") {
+    		ele.style.display = "none";
+		text.innerHTML = "Подробнее";
+  	}
+	else {
+		ele.style.display = "block";
+		text.innerHTML = "Скрыть";
+	}
+}
 
